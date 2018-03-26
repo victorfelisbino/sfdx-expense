2ab421e6bb00d924f1482934b3bf56d928b6dc1a
Victor <victorfelisbino@gmail.com>
2018-03-26 15:16:54 -0700

    Added view button to expense cards

--- Expense/main/default/aura/ExpenseCard/ExpenseCardController.js
+++ Expense/main/default/aura/ExpenseCard/ExpenseCardController.js
@@ -1,4 +1,11 @@
 ({
+    navigateToExpense: function(component, event) {
+        var expense = component.get("v.expense");
+        $A.get("e.force:navigateToSObject").setParams({
+            "recordId": expense.Id
+        }).fire();
+    },
+
     onValueChange: function(component, event) {
         var expense = component.get("v.expense");
         var updateEvent = component.getEvent("expenseUpdatedEvent");
